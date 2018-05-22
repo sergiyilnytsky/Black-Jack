0bd725f4d88e76a1ec9fd4d10afcf719868fb163
Sergiy Ilnytsky <sergiyilnytsky@gmail.com>
2018-05-22 20:27:37 +0300

    Two enums for card rank and card suits was created

---
+++ Black Jack/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+enum CardSuit	// масти для карт
+{
+	Club,		//трефы
+	Diamond,	//бубны
+	Heart,		//червы
+	Peak,		//пики
+	MAX_SUITS	//количество перечислений
+};
+
+enum CardRank	//достоинства карт
+{
+	Rank_2,
+	Rank_3,
+	Rank_4,
+	Rank_5,
+	Rank_6,
+	Rank_7,
+	Rank_8,
+	Rank_9,
+	Rank_10,
+	Rank_Jack,		//Валет
+	Rank_Queen,		//Дама
+	Rank_King,		//Король
+	Rank_Ace,		//Туз
+	MAX_RANKS
+};
